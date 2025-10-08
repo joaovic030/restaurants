b73739780c0e1c66dc1383319fa31ee54e472ba3
João Victor Rocha <joaovic.rocha3@gmail.com>
2025-10-08 11:03:32 -0400

    feat: adjust associations and uniqueness menu item name

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,4 +3,19 @@ class MenuItem < ApplicationRecord
   belongs_to :menu_group
 
   validates_presence_of :name, :description, :price, :cost
+  validates_uniqueness_of :name
+
+
+  delegate :restaurant_id, to: :menu
+
+  # An approach to validate name uniqueness based on the restaurant. Or perhaps validate it through a validates uniqueness via scope.
+  # private
+
+  # def name_uniqueness_within_restaurant
+  #   return unless menu && name.present?
+
+  #   if MenuItem.joins(:menu).where(name: name, menus: { restaurant_id: menu.restaurant_id }).where.not(id: id).exists?
+  #     errors.add(:name, "must be unique within the same restaurant")
+  #   end
+  # end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
   validates_presence_of :name
+
+  has_many :menus
 end
